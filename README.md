bebe2f39a6ac7bc1f8751c1ee7bc494c319e387f
freshmea <freshmea@naver.com>
2023-09-27 17:13:32 +0200

    google api

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # digitalphoto_project
 making of digital photo program
+
+- - -
+## 2023_9_27
+- - -
+* 의뢰한 나무 액자 완성
+* 전원 연결 및 젯슨 나노 테스트
+* 기본 아이디 jetson/jetson
+* gthumb 프로그램으로 사진 연속으로 나오게 하기 
+* google photo api 연동 
+  * 240566316207-lcq43h4vipivrg519crr66pjeqgrbreo.apps.googleusercontent.com
+  * credential 파일 생성. google cloud
+* 사람이 없을 때는 화면 꺼짐과 최대절전모드 + pir 센서로 화면 켜짐 정상 작동 모드로 진입하기
+* 개발 환경 세팅
+  * vscode 설치
+  * 노트북에서 vscode ssh로 접근 깃허브 푸쉬/풀 작동확인
+  * pytorch, cuda, opencv 설치 및 버전 확인 
+* 추가 부품 
+  * 전원 연결 어댑터 7V 4A 젯슨 나노용
+  * 벽에 설치시 필요한 전선, 익스텐션
---
+++ photo_api.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import os, time, sys, datetime
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+import wget
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/photoslibrary",
+          "https://www.googleapis.com/auth/photoslibrary.readonly",
+          "https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata"]
+
+def main():
+    #OAuth2 authentication process
+    print("OAuth2 authentication process...")
+
+    store = file.Storage("token.json")
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build("photoslibrary", "v1", http=creds.authorize(Http()))
+
+main()
